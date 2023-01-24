200399c8dfc69fe6a09713018a56717d69f043ee
Dima Sribnyj <dimasribnyj12@gmail.com>
2023-01-24 21:01:00 +0200

    nenavishu

--- Practice/Third Course Logika/Tasks-24.01/script.js
+++ Practice/Third Course Logika/Tasks-24.01/script.js
@@ -1,8 +1,13 @@
-const input = document.createElement(tag = "input", properties = [["innerHTML", "Lorem ipsum"], ["type", "text"], ["placeholder", "dolor sit amet"]], styles = [["fontSize", "18px"], ["color", "red"]]);
+const customElement = document.createElement(tag = 'input', properties = [['innerHTML', 'Lorem Ipsum'], ['type', 'text'], ["placeholder", "nenavishu eto zadanie"]], styles = [['color', 'red'], ['fontSize', '24px']], parentEl = document.querySelector(".Rock"));
+const path = parentEl;
+console.log("Defined or not?", `${properties[1][1]}`, `${styles[0][1]}`, `${properties[2][0]}`, properties[2]);
 for (let propertie of properties) {
-    propertie.style = [['fontSize', '72px'], ['backgroundColor', 'yellow']];
-    console.log("rock")
+    customElement.setAttribute(`${propertie[0][0]}`, `${propertie[0][1]}`);
+    customElement.setAttribute(`${propertie[1][0]}`, `${propertie[1][1]}`);
+    customElement.setAttribute(`${properties[2][0]}`, `${properties[2][1]}`);
 }
-console.log("checking append")
-const rock = document.querySelector('.Rock');
-rock.append(input);
+for (let design of styles) {
+    customElement.style.color = "red"; //`${design[0][1]}`
+    customElement.style.fontSize = "24px"; // `${design[1][1]}`
+}
+path.append(customElement)
