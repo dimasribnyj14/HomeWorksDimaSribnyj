892b7496d729861e2e4a9196b0eb31c2204ae390
Dima Sribnyj <dimasribnyj12@gmail.com>
2023-02-01 15:17:37 +0200

    Task01022023

--- Practice/Third Course Logika/Tasks-01.02/01.02/script.js
+++ Practice/Third Course Logika/Tasks-01.02/01.02/script.js
@@ -1,14 +1,13 @@
 // Функция которая изменяет цвет исходя из атрибута value у option
 function changeColor(event) {
     const bodyElem = document.querySelector("body");
-    bodyElem.style.backgroundColor = selectColorEvent;
+    bodyElem.style.backgroundColor = selectColor.value;
     console.log(selectColor.value);
 }
 // Сюда пишите название события, которое должно произойти после смены option
-const selectColor = document.getElementById('selectColor');
-var selectColorEvent = `${selectColor.value}`;
+const selectColor = document.querySelector('select');
+const selectColorEvent = "change";
 // Создаем события на тег select.
 selectColor.addEventListener(selectColorEvent, changeColor);
-console.log(selectColor.value);
 // Напишите, почему событие делаем на тег select, а работаем с тегом option?
 // Здесь писать.
