3cf798d78b83dd4f724c1bb10042b38f9d7e9ad4
doctor85 <dimasribnyj12@gmail.com>
2023-02-27 21:01:48 +0300

    IntensivUpdate

--- Other/dochak_tm/games/views.py
+++ Other/dochak_tm/games/views.py
@@ -4,6 +4,6 @@ from games.models import *
 def show_release_year_page(request):
     list_years = Release_Year.objects.all()
     return render(request,'release_years.html',context={'list_years':list_years})
-def show_main_page(request,games_pk):
-    list_games = Game.objects.filter(games_id=games_pk)
+def show_main_page(request,games_pk=id):
+    list_games = Game.objects.filter(id=games_pk)
     return render(request,'main.html',context={'list_games':list_games})
