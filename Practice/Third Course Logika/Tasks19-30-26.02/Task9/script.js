02ae65ac926af9eddab878eec035d47df5912fde
Dima Sribnyj <dimasribnyj12@gmail.com>
2023-03-02 11:02:20 +0200

    CommentTask9

--- Practice/Third Course Logika/Tasks19-30-26.02/Task9/script.js
+++ Practice/Third Course Logika/Tasks19-30-26.02/Task9/script.js
@@ -1,28 +1,28 @@
-// Подключаем div .reg
+// Подключаем #reg и .input
 const form = document.querySelector("#reg");
 let inputed = document.querySelectorAll('.input');
+// inputed[0].value равен твоему имя пользователя
+// inputed[1].value равен твоей електронной почты
+// inputed[2].value равен твоего пароля
 // Добавляем LocalStorage
-let myStorage = window.localStorage;
-// Список запрещенных слов
-const bannedWords = ['%', '^', '&', '*', ";", ':', '!', '@', '#', "$", '~', '`', '"', "'", "/", '?', '.', ',', '>', "<"]
-// Отправляем запрос на форму
-console.error(myStorage);
+let myStorage = window.localStorage; // ВАЖНО! Создаем localStorage при помощи window
+// Список запрещенных символов
+const bannedSymbols = ['%', '^', '&', '*', ";", ':', '!', '@', '#', "$", '~', '`', '"', "'", "/", '?', '.', ',', '>', "<"]
+// Сохраняем данные перед уходом
 window.onbeforeunload = function () {
-    myStorage.setItem('username', inputed[0].value);
-    myStorage.setItem('email', inputed[1].value);
-    myStorage.setItem('password', inputed[2].value);
-    return "Есть несохранённые изменения. Всё равно уходим?";
-
+    myStorage.setItem('username', inputed[0].value); //Добавляем имя пользователя в localStorage
+    myStorage.setItem('email', inputed[1].value); //Добавляем электронную почту в localStorage
+    myStorage.setItem('password', inputed[2].value); //Добавляем пароль в localStorage
 };
-window.onload = function () { // можно также использовать window.addEventListener('load', (event) => {
-    inputed[0].value = myStorage.getItem('username');
-    inputed[1].value = myStorage.getItem('email');
-    inputed[2].value = myStorage.getItem('password');
+// Загружаем данные из localStorage при заходе на страницу 
+window.onload = function () {
+    inputed[0].value = myStorage.getItem('username'); // Вставляем имя пользователя из localStorage в input 
+    inputed[1].value = myStorage.getItem('email'); // Вставляем электронную почту из localStorage в input
+    inputed[2].value = myStorage.getItem('password'); // Вставляем пароль из localStorage в input
 };
 form.addEventListener("submit", (event) => {
     // Очищаем настройки элемента браузера
     event.preventDefault();
-
     // Подключаем все классы input
     let inputs = document.querySelectorAll('.input');
     // Подключаем все классы error
@@ -42,14 +42,12 @@ form.addEventListener("submit", (event) => {
     }
     // Если результат равен true то отправляем форму
     if (result == true) {
-        form.submit()
-        inputs[0].value = ''
-        inputs[1].value = ''
-        inputs[2].value = ''
-        myStorage.clear()
+        form.submit() //Потверждаем отправку формы
+        inputs[0].value = '' //Очищаем input имя пользователя
+        inputs[1].value = '' //Очищаем input электронной почты
+        inputs[2].value = '' //Очищаем input пароля 
+        myStorage.clear() //Очищаем все данные localStorage
     }
-
-
 })
 // Создание ошибки
 function createError(text, elem) {
@@ -68,7 +66,7 @@ function createError(text, elem) {
     // Добавляем класс error к div'ам
     divError.classList.add('error');
 }
-// 
+// Функция валидации
 function validation(input) {
     // Если поле пустое
     if (input.value == '') {
@@ -82,13 +80,16 @@ function validation(input) {
         // Относимся к input.value с именем symbol
         for (let symbol of input.value) {
             // Если в symbol содержится запрещенные символы
-            if (bannedWords.includes(symbol)) {
+            if (bannedSymbols.includes(symbol)) {
                 // Отобразится текст, что введен недопустимый символ и символ который ты ввел
                 createError(`Введен недопустимый символ: ${symbol}`, input);
                 // Возращаемся с false
                 return false;
+                // Если в имени пользователя меньше 6 символов
             } else if (input.value.length < 6) {
+                // Отобразится текст, что имя пользователя должен содержать не менее 6 символов
                 createError('Ваше имя пользователя должен содержать не менее 6 символов', input);
+                // Возращаемся с false
                 return false;
             }
         }
@@ -103,23 +104,28 @@ function validation(input) {
             return false;
         }
     }
-    //
+    // Если id у input'а равен паролю
     else if (input.id == "password") {
         for (let symbol of input.value) {
+            // Если у пароля меньше 8 символов
             if (input.value.length < 8) {
+                // Отобразится текст, что пароль должен содержать не менее 8 символов
                 createError("Пароль должен содержать не менее 8 символов", input);
+                // Возращаемся с false
                 return false;
+                // Если у пароля нету цифр (Не работает)
             } else if (input.value.includes(Number)) {
+                // Отобразится текст, что пароль должен содержать как минимум 1 цифра
                 createError('Ваш пароль должен содержать как минимум 1 цифра', input);
+                // Возращаемся с false
                 return false;
+                // Если у пароля нету букв (Не работает)
             } else if (input.value.includes(Symbol)) {
+                // Отобразится текст, что пароль должен содержать как минимум 1 буква
                 createError('Ваш пароль должен содержать как минимум 1 буква', input);
+                // Возращаемся с false
                 return false;
-            } // else if (bannedWords.includes(symbol) == false) {
-            //     console.log(symbol)
-            //     createError('Ваш пароль должен содержать как минимум 1 символ', input);
-            //     return false;
-            // }
+            }
         }
     }
     // Возращаемся с true
