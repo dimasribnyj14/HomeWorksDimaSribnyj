d7a96e3f486ccbe9482de5453e8ae7df114cd3ca
dimasribnyj14 <dimasribnyj12@gmail.com>
2023-09-19 12:56:54 +0300

    nodejsfirstproject

---
+++ Practice/Course World It/FirstNodeJsProject/index.js
@@ -0,0 +1,6 @@
+let name = 'Дмитрий';
+let surname = 'Срібний';
+let age = 16;
+let hobby = 'програмувати';
+
+console.log(`Мені звати ${name} ${surname} мені ${age} років, у вільний час я полюбляю ${hobby}`);
